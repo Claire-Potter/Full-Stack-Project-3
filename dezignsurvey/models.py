8cac5e26dd03d7aefad80d583d7379b230140669
Claire Potter <clairepotter019@gmail.com>
2021-11-29 10:37:28 +0000

    Add models for dezignsurvey

--- dezignsurvey/models.py
+++ dezignsurvey/models.py
@@ -1,20 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 from cloudinary.models import CloudinaryField
 
-class Survey(models.Model):
-    survey_title = models.CharField(max_length=80)
-    survey_image = CloudinaryField('image', default='placeholder')
-    slug = models.SlugField(max_length=80, unique=True)
-    username = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="surveys",
-        default="1")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    created_on = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["-created_on"]
- 
-    def __str__(self):
-        return '%s' % (self.survey_title)
+class Survey(models.Model):
+    """A survey created by a user."""
+
+    title = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Question(models.Model):
+    """A question in a survey"""
+
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    prompt = models.CharField(max_length=128)
+
+
+class Option(models.Model):
+    """A multi-choice option available as a part of a survey question."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=128)
+
+
+class Submission(models.Model):
+    """A set of answers a survey's questions."""
+
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_complete = models.BooleanField(default=False)
+
+
+class Answer(models.Model):
+    """An answer a survey's questions."""
+
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
