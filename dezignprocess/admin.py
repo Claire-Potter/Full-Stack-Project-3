7fdc6c03d242f6339cb58696e4c0310aabd23099
Claire Potter <clairepotter019@gmail.com>
2021-12-18 18:31:23 +0000

    Update comments for dezignprocess templates

--- dezignprocess/admin.py
+++ dezignprocess/admin.py
@@ -7,7 +7,7 @@ The Step admin set up reads the Step model and allows
 the admin user to create a new step by adding the
 required fields. Admin can also edit and delete.
 The Tool admin set up reads the Tool model and allows
-the admin user to create a new step by adding the
+the admin user to create a new tool by adding the
 required fields. Admin can also edit and delete.
 The Comment admin set up reads the Comment model and allows
 the admin user to delete comments. No approval of comments
@@ -19,6 +19,12 @@ status is required as user progress status will only display to the
 user themself. The default 'Not Started' progress status was
 captured by the admin user from this screen. No additional
 progress statuses should be captured by the admin user.
+The Resource admin set up reads the Resource model and allows
+the admin user to create a new Resource by adding the
+required fields. Admin can also edit and delete.
+The Images admin set up reads the Images model and allows
+the admin user to create a new Image by adding the
+required fields. Admin can also edit and delete.
 """
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
@@ -107,11 +113,9 @@ class ResourceAdmin(AdminVideoMixin, admin.ModelAdmin):
 @admin.register(Images)
 class ImageAdmin(AdminVideoMixin, admin.ModelAdmin):
     """
-    The Resource admin set up reads the Resource model and allows
-    the admin user to create a new Resource by adding the
+    The Images admin set up reads the Images model and allows
+    the admin user to create a new Image by adding the
     required fields. Admin can also edit and delete.
-    AdminVideoMixin creates a preview od the video
-    Embed video is utilised to store the video.
     """
     list_display = ('category', 'title', 'name')
     search_fields = ['title', 'name']
--- dezignprocess/urls.py
+++ dezignprocess/urls.py
@@ -1,8 +1,12 @@
 """
 Xperiencedezignwiz dezignprocess app URL Configuration
-
 URLs for the dezignprocess app setup according to dezignprocess/views.py
-
+URLs configured for the following pages:
+First - the first list of steps
+Next - the second list of steps
+Search - the search results when a user searches for a step
+Step_Detail - the step detail page accessed from the step
+Step_Tools - the step tool page accessed from step_detail
 """
 from django.urls import path
 from dezignprocess import views
